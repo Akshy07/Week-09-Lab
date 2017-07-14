069addfbfeeb0ad41fcf699a3385fc3e795042ac
Akshay Patel <apate583@my.centennialcollege.ca>
2017-07-13 22:50:49 -0400

    Created Human Abstract Class.

---
+++ ConsoleApplication1/ConsoleApplication1/Human.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Akshay Patel
+ * Date: 11th July, 2017
+ * Description: THis is the Abstract Human Class.
+ */
+
+namespace Week09
+{
+    /// <summary>
+    /// This is the Human Abstract Class.
+    /// </summary>
+    public abstract class Human
+    {
+        //Private Fields (Instance Variables)
+        private string _name;
+
+        //Public Properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        } 
+
+        // Constructor ----------------------------------
+
+        /// <summary>
+        /// This is the cunstructor for the Human Abstract Class.
+        /// </summary>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
