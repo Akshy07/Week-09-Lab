d95caaa596ee432f519629e519de195fc6e4499e
Akshay Patel <apate583@my.centennialcollege.ca>
2017-07-13 23:02:25 -0400

    Added Power class

---
+++ ConsoleApplication1/ConsoleApplication1/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Akshay Patel
+ * Date: 11th July, 2017
+ * Description: THis is the new Power Data Type.
+ */
+
+namespace Week09
+{
+    /// <summary>
+    /// The Power class contains the data and structure of the each Super Power.
+    /// </summary>
+    public class Power
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
