4bd43aa84f82213a0b46de4b51626d689a365b43
Akshay Patel <apate583@my.centennialcollege.ca>
2017-07-13 22:58:30 -0400

    Added inherited subclass SuperHuman.

---
+++ ConsoleApplication1/ConsoleApplication1/SuperHuman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Akshay Patel
+ * Date: 11th July, 2017
+ * Description: THis is the SuperHuman sub Class.
+ */
+
+namespace Week09
+{
+    /// <summary>
+    /// This is the SuperHuman Sub Class.
+    /// It is inherited from the Human SuperClass.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private Fields
+
+        //Public Properties
+
+        //Constructor
+        public SuperHuman(string name) : base(name)
+        {
+
+        }
+        //Public Method
+    }
+}
